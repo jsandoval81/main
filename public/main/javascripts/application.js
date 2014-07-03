bc5413c21c1591b5928aede30db3b93a7269e2a2
jsandoval <jsandoval81@users.noreply.github.com>
2014-07-02 22:35:18 -0600

    Release 0.1

---
+++ public/main/javascripts/application.js
@@ -0,0 +1,12 @@
+
+//======================
+//== GLOBAL VARIABLES ==
+//======================
+	//== Store the parsable page URI
+	var pageURL = $.url();
+
+//======================
+//== NAVIGATION MENUS ==
+//======================
+
+
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'www.johnsandoval.us' });
+  res.render('index.ejs', { title: 'www.johnsandoval.us' });
 });
 
 module.exports = router;
