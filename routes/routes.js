dd47bc27f82a454c48034e7b30edfeb4510902c3
jsandoval <jsandoval81@users.noreply.github.com>
2014-07-09 22:09:41 -0600

    Release 0.1

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res) {
   res.render('index.ejs', { title: 'www.johnsandoval.us' });
+});
+/* GET journey page */
+router.get('/journey', function(req, res) {
+  res.render('journey.ejs', { title: 'www.johnsandoval.us' });
 });
 
 module.exports = router;
